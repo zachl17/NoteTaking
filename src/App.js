63630b3df1e9a61150111e6c10cb325532a60427
Zach Luedtke <zachluedtke17@gmail.com>
2020-01-25 18:55:07 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Duly Noted
         </p>
         <a
           className="App-link"
